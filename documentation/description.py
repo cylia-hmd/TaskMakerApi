ad759dd9ef4f484955972aff9f26b695439a7974
cylia-hmd <77634491+cylia-hmd@users.noreply.github.com>
2023-10-17 10:37:11 +0200

    Api description modified

--- documentation/description.py
+++ documentation/description.py
@@ -1,13 +1,11 @@
 api_description = """
-# Description de l'Application de Gestion des Tâches pour Étudiants
+# Description de l'Application de Gestion des Tâches.
 
-L'application de gestion des tâches pour étudiants est conçue pour aider les étudiants à gérer leurs devoirs, rappels et calendrier académique. Elle offre les fonctionnalités suivantes :
+Une application de gestion des tâches conçue pour aider les users à gérer leurs devoirs et rappels. Elle offre les fonctionnalités suivantes :
 
-- Création de tâches : Les étudiants peuvent créer des tâches spécifiques, telles que des devoirs, des projets ou des activités académiques.
-- Gestion des rappels : Les étudiants peuvent définir des rappels pour leurs tâches afin de ne rien oublier.
-- Calendrier académique : L'application permet aux étudiants de suivre leur calendrier académique, y compris les dates d'examens, devoirs, et autres événements.
-- Suivi de l'état de la tâche : Les étudiants peuvent marquer une tâche comme complète ou incomplète.
-- Organisation des tâches par catégories : Les tâches peuvent être organisées par catégories, telles que matières, projets, ou autres.
+- Création de tâches : Les users peuvent créer des tâches spécifiques, telles que des devoirs, des projets ou des activités académiques.
+- Calendrier académique : L'application permet aux users de suivre leur calendrier académique, y compris les dates d'examens, devoirs, et autres événements.
+- Suivi de l'état de la tâche : Les users peuvent marquer une tâche comme complète ou incomplète.
 - Gestion des utilisateurs : L'application prend en charge l'inscription d'utilisateurs et la gestion de leurs comptes.
 
 
