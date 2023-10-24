623387aac824a128b87bfafc0a974c1a56276bff
cylia-hmd <77634491+cylia-hmd@users.noreply.github.com>
2023-10-24 17:54:48 +0200

    files updated

---
+++ configs/firebase_config_example.py
@@ -0,0 +1,9 @@
+firebaseConfig = {
+  "apiKey": "AIzaSyD8hg-azImSaBOWyM4uFKyHJ_AzCeA6exs",
+  "authDomain": "taskmakerapi.firebaseapp.com",
+  "projectId": "taskmakerapi",
+  "storageBucket": "taskmakerapi.appspot.com",
+  "messagingSenderId": "938335966415",
+  "appId": "1:938335966415:web:28f9632b5b8024e20413ba",
+  "measurementId": "G-HHBTK2R0VL"
+}
